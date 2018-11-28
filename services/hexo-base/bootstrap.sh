43da24537a4771afb40bb39f0e207c9501d2a4f8
Naoaki Obiki <obiki.wk@gmail.com>
2018-11-28 18:04:05 +0900

    :art: refactor

--- services/hexo-base/bootstrap.sh
+++ services/hexo-base/bootstrap.sh
@@ -6,17 +6,6 @@ if [ ! -e /web/package.json ]; then
     "dependencies": {
       "hexo": "^3.2.0",
       "hexo-cli": "^1.0.4",
-      "hexo-generator-archive": "^0.1.4",
-      "hexo-generator-category": "^0.1.3",
-      "hexo-generator-feed": "^1.2.2",
-      "hexo-generator-robotstxt": "^0.2.0",
-      "hexo-generator-seo-friendly-sitemap": "0.0.21",
-      "hexo-generator-index": "^0.2.0",
-      "hexo-generator-tag": "^0.2.0",
-      "hexo-renderer-ejs": "^0.3.0",
-      "hexo-renderer-less": "^0.2.0",
-      "hexo-renderer-marked": "^0.3.0",
-      "hexo-renderer-stylus": "^0.3.1",
       "hexo-server": "^0.2.0"
     }
   }
@@ -30,6 +19,7 @@ if [ -z "`ls /web/site`" ];then
   cd /web/site && hexo init
 fi
 
+cd /web/site && yarn install
 cd /web/site && hexo generate
 chown -R 1000:1000 /web
 
