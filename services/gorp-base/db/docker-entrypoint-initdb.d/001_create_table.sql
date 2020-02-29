c6245b6d6c5e7de754f8bcc807ba2d81e3bc1edc
Naoaki Obiki <obiki.wk@gmail.com>
2020-02-29 21:04:02 +0900

    postgres

--- services/gorp-base/db/docker-entrypoint-initdb.d/001_create_table.sql
+++ services/gorp-base/db/docker-entrypoint-initdb.d/001_create_table.sql
@@ -1,7 +1,10 @@
-BEGIN;
-CREATE TABLE hoge
+begin;
+create table hoge
 (
-    id int,
-    name varchar
+    id serial not null,
+    name varchar(255)
 );
-COMMIT;
+insert into hoge (name) values ('hello1');
+insert into hoge (name) values ('hello3');
+insert into hoge (name) values ('hello2');
+commit;
